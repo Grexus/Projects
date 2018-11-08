cb48e51fd9ac89d2b8c8fe4e14687ddaeae20dd8
Grexus <matthew-grech@outlook.com>
2018-11-09 00:05:31 +0100

    started logic for to-do list

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
 export class App {
   constructor() {
-    this.message = 'Hello World!';
+    this.appName = 'To-Do List';
+    this.userInput = '';
+    this.toDos = [];
+  }
+
+  addToList(){
+    let newToDo = {};
+
+    newToDo.description = this.userInput;
+    newToDo.isDone = false;
+    this.toDos.push(newToDo);
+    this.userInput = '';
+  }
+
+
+  // Finish Edit Function ! ! !
+  editToDo(i){
+    this.toDos[i].description = 'edited';
+  }
+
+  toggleDone(i){
+    // this.toDos[i].isDone = !this.toDos[i].isDone;
+
+    if(this.toDos[i].isDone){
+      this.toDos[i].isDone = false;
+    } else {
+      this.toDos[i].isDone = true;
+    }
+  }
+
+  remove(i){
+    this.toDos.splice(i, 1);
   }
 }
---
+++ src/todo-list/todo-list.js
@@ -0,0 +1,5 @@
+export class toDoList {
+  constructor() {
+    this.appName = 'To-Do List';
+  }
+}
